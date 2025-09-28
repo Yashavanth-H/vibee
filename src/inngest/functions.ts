70dca872472f19f1616595a70061e3b613c90e2a
Yashavanthh <g.h.yashavanth@gmail.com>
2025-09-28 19:00:00 +0530

    06:e2b Sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,24 +1,35 @@
-import { gemini, createAgent } from "@inngest/agent-kit"
+import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-testyash-2");
+      return sandbox.sandboxId;
+    });
 
-    const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer.  You summarize in two words.",
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code. You write simople Next.js & React snippets",
       model: gemini({ model:"gemini-2.5-pro"}),
     });
 
-    const { output } = await summarizer.run(
-    `Summarize the following the text: ${event.data.value}`,
+    const { output } = await codeAgent.run(
+    `Write the folowing snippet: ${event.data.value}`,
     );
 
-    console.log(output);
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
-    return { output };
+
+    return { output , sandboxUrl};
   },
 );
