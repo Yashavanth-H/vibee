91507bd94889256cf1add9715ce69c06fdfe1c8c
Yashavanthh <g.h.yashavanth@gmail.com>
2025-11-23 11:34:38 +0530

    feat: Update AI agent model to deepseek/deepseek-chat and enable message fetching in `getMany` procedure.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,14 @@
-import { gemini,openai, createAgent, createTool, createNetwork, type Tool, type Message, createState} from "@inngest/agent-kit";
+import { gemini, openai, createAgent, createTool, createNetwork, type Tool, type Message, createState } from "@inngest/agent-kit";
 import { inngest } from "./client";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox, lastAssistantTextMessageContent, parseAgentOutput } from "./utils";
 import z from "zod";
-import { PROMPT, FRAGMENT_TITLE_PROMPT,RESPONSE_PROMPT } from "@/prompt";
+import { PROMPT, FRAGMENT_TITLE_PROMPT, RESPONSE_PROMPT } from "@/prompt";
 import { prisma } from "@/lib/db";
 
 interface AgentState {
   summary: string;
-  files: {[path: string]: string};
+  files: { [path: string]: string };
 }
 
 
@@ -17,7 +17,7 @@ interface AgentState {
 export const codeAgentFunction = inngest.createFunction(
   { id: "cde-agent" },
   { event: "code-agent/run" },
-  
+
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("vibe-nextjs-testyash-2");
@@ -66,7 +66,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "An expert coding agent",
       system: PROMPT,
       model: openai({
-        model: "minimax/minimax-m2:free",
+        model: "deepseek/deepseek-chat",
         apiKey: process.env.OPENROUTER_API_KEY,
         baseUrl: "https://openrouter.ai/api/v1",
       }),
@@ -79,9 +79,10 @@ export const codeAgentFunction = inngest.createFunction(
           }),
           handler: async ({ command }, { step }) => {
             return await step?.run("terminal", async () => {
-              const buffers = { stdout: "", stderr: ""};
+              const buffers = { stdout: "", stderr: "" };
 
-              try { const sandbox = await getSandbox(sandboxId);
+              try {
+                const sandbox = await getSandbox(sandboxId);
                 const result = await sandbox.commands.run(command, {
                   onStdout: (data: string) => {
                     buffers.stdout += data;
@@ -91,7 +92,7 @@ export const codeAgentFunction = inngest.createFunction(
                   }
                 });
                 return result.stdout;
-              } catch(e){
+              } catch (e) {
                 console.error(
                   `command failed : ${e} \nstdout: ${buffers.stdout} \nstderror: ${buffers.stderr}`,
                 );
@@ -111,59 +112,59 @@ export const codeAgentFunction = inngest.createFunction(
               }),
             ),
           }),
-          handler: async(
-            {files},
-            {step, network}: Tool.Options<AgentState>
+          handler: async (
+            { files },
+            { step, network }: Tool.Options<AgentState>
           ) => {
             const newFiles = await step?.run("createOrUpdateFiles", async () => {
               try {
                 const updatedFiles = network.state.data.files || {};
                 const sandbox = await getSandbox(sandboxId);
-              for ( const file of files){
-                await sandbox.files.write(file.path, file.content);
-                updatedFiles[file.path] = file.content;
-              }
-              return updatedFiles;
+                for (const file of files) {
+                  await sandbox.files.write(file.path, file.content);
+                  updatedFiles[file.path] = file.content;
+                }
+                return updatedFiles;
               } catch (e) {
                 return "Error: " + e;
               }
             });
 
-            if(typeof newFiles === "object"){
+            if (typeof newFiles === "object") {
               network.state.data.files = newFiles;
             }
           }
-          }),
+        }),
         createTool({
-            name: "readFiles",
-            description: "Read files from the sandbox",
-            parameters: z.object({
-              files: z.array(z.string()),
-            }),
-            handler: async ({ files}, { step }) => {
-              return await step?.run("readFiles", async () => {
-                try {
-                  const sandbox = await getSandbox(sandboxId);
-                  const contents = [];
-                  for (const file of files) {
-                    const content = await sandbox.files.read(file);
-                    contents.push({ path: file, content });
-                  }
-                  return JSON.stringify(contents);
-                } catch (e) {
-                  return "Error " + e;
-                }
-              })
-            }
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          parameters: z.object({
+            files: z.array(z.string()),
           }),
+          handler: async ({ files }, { step }) => {
+            return await step?.run("readFiles", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const contents = [];
+                for (const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({ path: file, content });
+                }
+                return JSON.stringify(contents);
+              } catch (e) {
+                return "Error " + e;
+              }
+            })
+          }
+        }),
       ],
       lifecycle: {
         onResponse: async ({ result, network }) => {
-          const lastAssistantMessageText = 
-          lastAssistantTextMessageContent(result);
+          const lastAssistantMessageText =
+            lastAssistantTextMessageContent(result);
 
-          if(lastAssistantMessageText && network) {
-            if(lastAssistantMessageText.includes("<task_summary>")) {
+          if (lastAssistantMessageText && network) {
+            if (lastAssistantMessageText.includes("<task_summary>")) {
               network.state.data.summary = lastAssistantMessageText;
             }
           }
@@ -181,7 +182,7 @@ export const codeAgentFunction = inngest.createFunction(
       router: async ({ network }) => {
         const summary = network.state.data.summary;
 
-        if(summary) {
+        if (summary) {
           return;
         }
 
@@ -209,26 +210,26 @@ export const codeAgentFunction = inngest.createFunction(
       }),
     });
 
-    const { 
-      output: fragmentTitleOutput 
+    const {
+      output: fragmentTitleOutput
     } = await fragmentTitleGenerator.run(result.state.data.summary);
-    const { 
-      output: responseOutput 
+    const {
+      output: responseOutput
     } = await responseGenerator.run(result.state.data.summary);
 
-    const isError = 
-    !result.state.data.summary ||
-    Object.keys(result.state.data.files || {}).length === 0;
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length === 0;
 
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
-      const host =  sandbox.getHost(3000);
+      const host = sandbox.getHost(3000);
       return `https://${host}`;
     });
 
 
     await step.run("save-result", async () => {
-      if(isError) {
+      if (isError) {
         return await prisma.message.create({
           data: {
             projectId: event.data.projectId,
@@ -247,7 +248,7 @@ export const codeAgentFunction = inngest.createFunction(
           role: "ASSISTANT",
           type: "RESULT",
           fragment: {
-            create:{
+            create: {
               sandboxUrl: sandboxUrl,
               title: parseAgentOutput(fragmentTitleOutput),
               files: result.state.data.files,
--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -7,13 +7,13 @@ import { consumeCredits } from "@/lib/usage";
 
 export const messagesRouter = createTRPCRouter({
     getMany: protectedProcedure
-    .input(
-        z.object({
-        projectId: z.string().min(1, { message: "Project Id is required"}),
-        }),
-    )
-        .query(async ({input, ctx}) => {
-            throw new TRPCError({code: "BAD_REQUEST"});
+        .input(
+            z.object({
+                projectId: z.string().min(1, { message: "Project Id is required" }),
+            }),
+        )
+        .query(async ({ input, ctx }) => {
+            // throw new TRPCError({code: "BAD_REQUEST"});
 
             const messages = await prisma.message.findMany({
                 where: {
@@ -34,57 +34,57 @@ export const messagesRouter = createTRPCRouter({
         }),
 
     create: protectedProcedure
-    .input(
-        z.object({
-            value: z.string()
-            .min(1, { message: "Value is required"})
-            .max(10000,{ message: "Value is too long"}),
-        projectId: z.string().min(1, { message: "Project Id is required"}),
+        .input(
+            z.object({
+                value: z.string()
+                    .min(1, { message: "Value is required" })
+                    .max(10000, { message: "Value is too long" }),
+                projectId: z.string().min(1, { message: "Project Id is required" }),
+            }),
+        )
+
+        .mutation(async ({ input, ctx }) => {
+            const existingProject = await prisma.project.findUnique({
+                where: {
+                    id: input.projectId,
+                    userId: ctx.auth.userId,
+                },
+            });
+
+            if (!existingProject) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Project Not Found" });
+            }
+
+            try {
+                await consumeCredits();
+            } catch (error) {
+                if (error instanceof Error) {
+                    throw new TRPCError({ code: "BAD_REQUEST", message: "Something went wrong" });
+                } else {
+                    throw new TRPCError({
+                        code: "TOO_MANY_REQUESTS",
+                        message: "You have run out of credits"
+                    })
+                }
+            }
+
+            const createMessage = await prisma.message.create({
+                data: {
+                    projectId: input.projectId,
+                    content: input.value,
+                    role: "USER",
+                    type: "RESULT",
+                },
+            });
+
+            await inngest.send({
+                name: "code-agent/run",
+                data: {
+                    value: input.value,
+                    projectId: input.projectId,
+                }
+            });
+
+            return createMessage;
         }),
-    )
-
-    .mutation(async ({ input, ctx }) => {
-        const existingProject = await prisma.project.findUnique({
-            where: {
-                id: input.projectId,
-                userId: ctx.auth.userId,
-            },
-        });
-
-        if(!existingProject){
-            throw new TRPCError({ code: "NOT_FOUND", message: "Project Not Found"});
-        }
-
-        try {
-            await consumeCredits();
-        } catch (error) {
-            if(error instanceof Error) {
-                throw new TRPCError ({ code: "BAD_REQUEST", message: "Something went wrong"});
-            } else {
-                throw new TRPCError ({
-                    code: "TOO_MANY_REQUESTS",
-                    message: "You have run out of credits"
-                })
-            }
-        }
-
-        const createMessage = await prisma.message.create({
-            data: {
-                projectId: input.projectId,
-                content: input.value,
-                role: "USER",
-                type: "RESULT",
-            },
-        });
-
-        await inngest.send({
-            name: "code-agent/run",
-            data: {
-                value: input.value,
-                projectId: input.projectId,
-            }
-        });
-
-        return createMessage;
-    }),
 });
